6ec0ecdbf6e20a85c82b1cfa61ee2ea32b1f8c95
Pramay_07 <76201352+Meet999000@users.noreply.github.com>
2023-03-28 11:45:54 +0530

    feat(authentication): api changes.

--- routes/userMaster.js
+++ routes/userMaster.js
@@ -1,12 +1,12 @@
 const router = require("express").Router()
 const userForm = require("../controllers/userForm")
-const {validate} = require('../middlewares/validate')
-const {auth} = require("../middlewares/auth")
+const { validate } = require('../middlewares/validate')
+const { auth } = require("../middlewares/auth")
 
 
-router.post("/create", validate("body",userForm.createForm.validation), userForm.createForm.handler)
-router.put("/update",auth, validate("body",userForm.updateUserForm.validation), userForm.updateUserForm.handler)
+router.post("/create", validate("body", userForm.createForm.validation), userForm.createForm.handler)
+router.put("/update", validate("body", userForm.updateUserForm.validation), userForm.updateUserForm.handler)
 router.get("/find", userForm.getUserForm.handler)
-router.delete("/delete", auth, userForm.deleteUserForm.handler)
+router.delete("/delete", userForm.deleteUserForm.handler)
 
 module.exports = router
